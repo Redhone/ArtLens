8a29b72bc123aff904c4e8c89e0dc479173faa1f
Redhone <blilitaradoine2001@gmail.com>
2025-08-12 20:14:56 +0200

    correcting positionn of the new model

--- app.js
+++ app.js
@@ -230,7 +230,7 @@ const MODELS = [
       alternate: {
         baseScale: 1,
         position: { x: 0, y: 0, z: 0 },
-        rotation: { x: 0, y: 45, z: 0 }
+        rotation: { x: 0, y: 15, z: 0 }
       }
     }
   },
